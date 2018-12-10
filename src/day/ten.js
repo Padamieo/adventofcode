05d7cd4d0465f58e0b85c931c587a5774c99845f
Padam <michaeladamlockwood@googlemail.com>
2018-12-10 23:53:44 +0100

    minor improvement too day 10

--- src/day/ten.js
+++ src/day/ten.js
@@ -4,8 +4,40 @@ const c = new common();
 
 export default function init() {
 	var data = processData(text);
-	console.log(data);
-	return 'ten';
+	var ranges = {};
+	ranges = generateRange(data, ranges, 'x');
+	ranges = generateRange(data, ranges, 'y');
+	console.log(ranges);
+	var map = [];
+	for(var y = 0; y < ranges.y.total; y++){
+		map.push([]);
+		for(var x = 0; x < ranges.x.total; x++){
+			map[y][x] = '0';
+		}
+	}
+	console.log(map);
+
+	// Object.keys(data).forEach(key => {
+	//
+	// });
+
+	return map.toString();
+}
+
+function generateRange(data, ranges, coord){
+	ranges[coord] = rangeFind(data, coord);
+	if(ranges[coord].min <= -1){
+		ranges[coord].offset = ranges[coord].min*-1;
+		ranges[coord].total = ranges[coord].offset+ranges[coord].max;
+	}
+	return ranges;
+}
+
+function rangeFind(data, subprop){
+	var array = Object.keys( data ).map(function ( key ) { return data[key].position[subprop]; });
+	var min = Math.min.apply( null, array );
+	var max = Math.max.apply( null, array );
+	return {min, max}
 }
 
 function processData(input) {
@@ -15,9 +47,11 @@ function processData(input) {
 		if(!c.empty(lines[i])){
 			var brackets = /<([^>]+)>/g;
 			var lineData = lines[i].match(brackets);
+			var position = lineData[0].replace(/\<|>|\s/g,'').split(',');
+			var velocity = lineData[1].replace(/\<|>|\s/g,'').split(',')
 			data[i] = {
-				position:lineData[0].replace(/\<|>|\s/g,'').split(','),
-				velocity:lineData[1].replace(/\<|>|\s/g,'').split(','),
+				position:{x:position[0],y:position[1]},
+				velocity:{x:velocity[0],y:velocity[1]}
 			};
 		}
 	}
