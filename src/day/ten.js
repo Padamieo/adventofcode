7675818fe875d9c2e19a981e7810f110938ef2b9
alockwood <adam@exmachina.nl>
2018-12-11 18:24:09 +0100

    added overworking to day 10

--- src/day/ten.js
+++ src/day/ten.js
@@ -4,40 +4,195 @@ const c = new common();
 
 export default function init() {
 	var data = processData(text);
-	var ranges = {};
-	ranges = generateRange(data, ranges, 'x');
-	ranges = generateRange(data, ranges, 'y');
-	console.log(ranges);
+
+	var found = findMoment(data);
+	console.log(found);
+	var newData = produce(data, found.at, found.info);
+
 	var map = [];
-	for(var y = 0; y < ranges.y.total; y++){
+	for(var y = 0; y <= found.info.y.total; y++){
 		map.push([]);
-		for(var x = 0; x < ranges.x.total; x++){
-			map[y][x] = '0';
+		for(var x = 0; x <= found.info.x.total; x++){
+			map[y][x] = '_';
 		}
 	}
-	console.log(map);
 
-	// Object.keys(data).forEach(key => {
-	//
-	// });
+	for(var i = 0; i < newData.length; i++){
+		var y = newData[i].y;
+		var x = newData[i].x;
+		map[y][x] = '#';
+	}
+	return visualise(map);
+}
 
-	return map.toString();
+function produce(data, i, info){
+	var newData = [];
+	Object.keys(data).forEach(key => {
+		var pos = calculateVelocityPosition(data[key], i);
+		var x = pos.x + info.x.offset;
+		var y = pos.y + info.y.offset;
+		newData.push({x,y});
+	});
+
+	console.log(findExitMoment(data));
+
+	return newData;
+}
+
+function findExitMoment(data){
+	var ranges = {};
+	var found = undefined;
+	var i = 0;
+	var bail = false;
+	var startRange = {};
+	var dataCopy = data;
+	while(bail === false){
+
+
+		if(i === 0){
+			var range = generateRangesArray(dataCopy, i);
+			var x = minMaxFind(range.x);
+			var y = minMaxFind(range.y);
+			startRange['x'] = x;
+			startRange['y'] = y;
+			//console.log(startRange, x, y);
+		}else{
+
+			Object.keys(dataCopy).forEach(key => {
+				var pos = calculateVelocityPosition(dataCopy[key], i);
+				// var remove = (
+				// 	(pos.y > startRange.y.max && pos.y < startRange.y.min)
+				// 	&&
+				// 	(pos.x > startRange.x.max && pos.x < startRange.x.min)
+				// );
+				var inXrange = (pos.x < startRange.x.max && pos.x > startRange.x.min);
+				var inYrange = (pos.y < startRange.y.max && pos.y > startRange.y.min);
+				if(inXrange == false || inYrange == false){
+					//console.log(pos, startRange);
+					//bail = true;
+					console.log(dataCopy);
+					delete dataCopy[key];
+				}
+			});
+
+			if(Object.keys(dataCopy).length <= 0){
+				console.log(dataCopy);
+				console.log(i);
+				found = i;
+				bail = true;
+			}
+		}
+
+
+
+		//console.log(x);
+		// if(x.min > startRange.x.max && y.min > startRange.y.max ){
+		// 	found = i;
+		// 	bail = true;
+		// }
+
+		// var area = x.total + y.total;
+		// ranges[i] = { x, y, area };
+		//
+		// if(i !== 0){
+		// 	if(ranges[i].area > ranges[i-1].area){
+		// 		bail = true;
+		// 		found = i-1;
+		// 	}
+		// }
+
+		if(i === 100000){
+			console.log(dataCopy);
+			bail = true;
+		}
+		i++;
+	}
+	return found;
+}
+
+function generateRangesArray(data, i){
+	var x = [];
+	var y = [];
+	Object.keys(data).forEach(key => {
+		var pos = calculateVelocityPosition(data[key], i);
+		x.push(pos.x);
+		y.push(pos.y);
+	});
+	return {x, y};
 }
 
 function generateRange(data, ranges, coord){
 	ranges[coord] = rangeFind(data, coord);
-	if(ranges[coord].min <= -1){
-		ranges[coord].offset = ranges[coord].min*-1;
-		ranges[coord].total = ranges[coord].offset+ranges[coord].max;
-	}
+	ranges[coord] = appendOffsetTotal(ranges[coord], coord);
 	return ranges;
 }
 
-function rangeFind(data, subprop){
-	var array = Object.keys( data ).map(function ( key ) { return data[key].position[subprop]; });
+function rangeFind(data, coord){
+	var array = Object.keys( data ).map(function ( key ) { return data[key].position[coord]; });
+	return minMaxFind(array);
+}
+
+function findMoment(data){
+	var ranges = {};
+	var found = undefined;
+	var i = 0;
+	var bail = false;
+	while(bail === false){
+
+		var range = generateRangesArray(data, i);
+		var x = appendOffsetTotal(minMaxFind(range.x));
+		var y = appendOffsetTotal(minMaxFind(range.y));
+
+		var area = x.total + y.total;
+		ranges[i] = { x, y, area };
+
+		if(i !== 0){
+			if(ranges[i].area > ranges[i-1].area){
+				bail = true;
+				found = i-1;
+			}
+		}
+		i++;
+	}
+	return {at:found, info:ranges[found]};
+}
+
+function calculateVelocityPosition(data, i){
+	var x = data.position.x;
+	if(i != 0){
+		var vx = data.velocity.x*i;
+		var x = x+vx;
+	}
+	var y = data.position.y;
+	if(i != 0){
+		var vy = data.velocity.y*i;
+		var y = y+vy;
+	}
+	return {x, y};
+}
+
+function visualise(map){
+	var string = '';
+	for(var y = 0; y < map.length; y++){
+		string += '<p>';
+		for(var x = 0; x < map[y].length; x++){
+			string += map[y][x];
+		}
+		string += '</p>';
+	}
+	return string;
+}
+
+function appendOffsetTotal(range){
+	range.offset = range.min*-1;
+	range.total = range.offset+range.max;
+	return range;
+}
+
+function minMaxFind(array){
 	var min = Math.min.apply( null, array );
 	var max = Math.max.apply( null, array );
-	return {min, max}
+	return {min, max};
 }
 
 function processData(input) {
@@ -50,8 +205,8 @@ function processData(input) {
 			var position = lineData[0].replace(/\<|>|\s/g,'').split(',');
 			var velocity = lineData[1].replace(/\<|>|\s/g,'').split(',')
 			data[i] = {
-				position:{x:position[0],y:position[1]},
-				velocity:{x:velocity[0],y:velocity[1]}
+				position:{x:parseInt(position[0]),y:parseInt(position[1])},
+				velocity:{x:parseInt(velocity[0]),y:parseInt(velocity[1])}
 			};
 		}
 	}
